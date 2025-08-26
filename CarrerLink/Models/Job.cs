89b78c31fa19eaf2cec8e650f56953d18c27f568
Nawaraj Neupane <nawarajneupane2003@gmail.com>
2025-08-26 09:50:03 +0545

    Job Model

---
+++ CarrerLink/Models/Job.cs
@@ -0,0 +1,17 @@
+﻿namespace CarrerLink.Models
+{
+    public class Job
+    {
+        public int JobId { get; set; }  // Primary Key
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string JobType { get; set; }  
+        public int Salary { get; set; }  
+        public DateTime PostedDate { get; set; }
+        public DateTime ApplicationDeadline { get; set; }
+
+        public int RecruiterId { get; set; }  // Linked to Recruiter
+        public Recruiter Recruiter { get; set; }  // Navigation Property
+    }
+}

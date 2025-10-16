f099977cb74e1574094abd0c8ebfee5efbc07154
Nawaraj Neupane <nawarajneupane2003@gmail.com>
2025-10-16 19:53:25 +0545

    JobSearch works

--- CarrerLink/Controllers/HomeController.cs
+++ CarrerLink/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics;
 using CarrerLink.Models;
-using CarrerLink.Data;  
+using CarrerLink.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,14 +17,46 @@ namespace CarrerLink.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        // GET: query string
+        public async Task<IActionResult> Index(string keyword, string jobType)
         {
-            var jobs = await _context.Job
-                                     .Include(j => j.Recruiter)
-                                         .ThenInclude(r => r.User)
-                                     .ToListAsync();
+            
+            var jobs = _context.Job
+                               .Include(j => j.Recruiter)
+                               .ThenInclude(r => r.User)
+                               .AsQueryable();
 
-            return View(jobs);  
+            
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                jobs = jobs.Where(j =>
+                    j.Title.Contains(keyword) ||
+                    j.Recruiter.CompanyName.Contains(keyword) ||
+                    j.Location.Contains(keyword));
+            }
+
+            
+            if (!string.IsNullOrEmpty(jobType))
+            {
+                jobs = jobs.Where(j => j.JobType == jobType);
+            }
+
+            // shows job types from db
+            var jobTypes = await _context.Job
+                                         .Select(j => j.JobType)
+                                         .Where(jt => !string.IsNullOrEmpty(jt))
+                                         .Distinct()
+                                         .OrderBy(jt => jt)
+                                         .ToListAsync();
+
+            ViewBag.JobTypes = jobTypes;
+
+            // sorting descending
+            var jobList = await jobs
+                                .OrderByDescending(j => j.PostedDate)
+                                .ToListAsync();
+
+            return View(jobList);
         }
 
         public IActionResult Privacy()
